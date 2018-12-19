ba6eb231318d4e8d49da5e0a2f6423973cde67f4
Netscylla <github@netscylla.com>
2018-12-19 13:51:35 +0000

    Update Phishing_Report.ps1

--- Phishing_Report.ps1
+++ Phishing_Report.ps1
@@ -1,32 +1,57 @@
 <#
 .Synopsis
-Phishing Report v0.1
+Phishing Report v0.2
 
 Author: Andy @ Netscylla (c) 2018
 License: GNU GPL v3
-Version: v0.1
+Version: v0.2
 
 .Description
 Simple script to collect email artifacts in specific OWA folder, currently: Email address, Name 
 #>
 
+param(  
+[Parameter(Position = 0, Mandatory = $false)] [string] $filename = "c:\Report.csv",      
+[Parameter(Position = 0, Mandatory = $false)] [string] $file = $false         
+)   
+ 
+#$search_folder = "\\Phishing\Phishing Campaign"
+$search_folder = Read-Host -Prompt 'Enter mail folder (ex: \\Phishing\Phishing Campaign)'
 $outlook = new-object -com outlook.application;
 $ns = $outlook.GetNameSpace("MAPI");
-
-#Find inbox with case/campaign
-#example = "\\Phishing\Phishing Campaign 1"
-#use following command to locate mailbox/folder: $ns.Folders[4].Folders | select folderpath;
-$inbox = $ns.Folders.Item(3).Folders.Item(6)
-
+$j=0
+for ($i=1; $i -lt 4; $i++){
+  $ns.Folders[$i].Folders|ForEach-Object{
+    $j+=1
+    if ($_.FolderPath -eq $search_folder){
+      $inbox = $ns.Folders.Item($i).Folders.Item($j)
+      Write-Host -ForegroundColor Green "Found"
+      break
+    }else{
+      Write-host -NoNewLine "."
+    }
+  }
+  $j=0
+}
+ 
+if (!$inbox){
+  exit
+}
+ 
+if ($file -eq $false){
+  Write-Host "Dumping to console"
+  Write-Host "Email,Surname,FirstName"
+}else{
+  Write-Host "Dumping to file " $filename
+}
+ 
 $inbox.items | foreach {
     if ($_.SenderEmailType.ToUpper().Equals("EX")){
       $recip = $ns.CreateRecipient($_.SenderName);
-      #if address entry exists continue
-      if ($recip.AddressEntry){     
+      if ($recip.AddressEntry){      
           $exUser = $recip.AddressEntry.GetExchangeUser();
           $smtpAddress = $exUser.PrimarySmtpAddress;
       }else{
-          #if address entry == null, guess at email address from name firstname.surname
           $namearray=@()
           $namearray=$_.SenderName.Split(" ,",[System.StringSplitOptions]::RemoveEmptyEntries)
           [array]::Reverse($namearray)
@@ -36,8 +61,11 @@ $inbox.items | foreach {
     }else{
       $smtpAddress = $_.SenderEmailAddress;
     }
-    "$smtpAddress,$($_.SenderName)" | out-file h:\test2.csv -Append
+ 
+    if($file -eq 1) {
+      "$smtpAddress,$($_.SenderName)" | out-file $filename -Append 
+    }else{
+      "$smtpAddress,$($_.SenderName)" |Format-Table -Autosize
+    }
     $smtpAddress=""
 }
-
-#$inbox.items| Select SenderEmailAddress,SenderName |Format-Table -AutoSize
